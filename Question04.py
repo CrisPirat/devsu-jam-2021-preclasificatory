cb038d46a4b2fb399ff1697a3ab5d685f62db8c5
unknown <=>
2021-09-03 12:56:50 -0500

    Exercise 4

--- Question04.py
+++ Question04.py
@@ -14,53 +14,70 @@ def isNumberConstraints(n):
         return True
     else :
         return False
-
-def getLetters(letters, indexes):
-    values = []
-    for i in indexes:
-        values.append(letters[i])
-    return values
-
-
-def sumSubIdx(comIdx, idx, max):
-    if len(comIdx) == idx:
-        comIdx.append(0)
-    if comIdx[idx]>max:
-        comIdx[idx]=0
-        return sumSubIdx(comIdx, idx + 1, max)
-    else: 
-        comIdx[idx] += 1
-        return comIdx
-
-
-
-def getCombinatory(letters,n):
-    sizeLetters = len(letters)-1
-    combinatoryIndexes = []
-    index = 0 
-    combinatoryIndexes.append(0)
-    while True:
-        if index == n :
-            return getLetters(combinatoryIndexes)
-        else:
-            subIndex = len(combinatoryIndexes)-1
-            if combinatoryIndexes[subIndex] < sizeLetters:
-                combinatoryIndexes[subIndex] += 1
+def checkVector(vector,max):
+    sizeV = len(vector)
+    for i in range(0,sizeV):
+        if vector[sizeV-1-i] > max:
+            vector[sizeV-1-i] = 0
+            if (sizeV-2-i) == -1:
+                vector.insert(0,0)
             else:
-                subIndex - 1
-        index += 1
+                vector[sizeV-2-i] += 1
+
+    return vector
+'''
+Recursion Block but "RecursionError: maximum recursion depth exceeded in comparison" is posible
+
+def getCombinatory(letters,n,result):
+    if len(result) == 0:
+        result.insert(0,0)
+    lsize = len(letters)
+    lresu = len(result)
+    if n>0 :
+        result[lresu-1] += 1
+        return getCombinatory(letters,n-1,checkVector(result,lsize-1))
+    else:    
+        resultString = ""
+        for r in result:
+            resultString += letters[r]
+        return resultString
+
+END Recursion Block
+'''
+def getCombinatory(letters,n,result):
+    if len(result) == 0:
+        result.insert(0,-1)
+    lsize = len(letters)
+    for i in range(0,n+1):
+        lresu = len(result)
+        result[lresu-1] += 1
+        result = checkVector(result,lsize-1)
+    resultString = ""
+    for r in result:
+        resultString += letters[r]
+    return resultString
 
 def customHeaderTitle(letters , n):
     if isEnglish(letters) and isUppercase(letters) and isNumberConstraints(n):
-        return getCombinatory(letters,n)
+        return getCombinatory(letters,n,[])
     return -1
 
 if __name__ == "__main__":
-    print(customHeaderTitle("AB",5))#Output BB
+    '''
+    for i in range(0,12):
+        print("n = {} -> {}".format(i,customHeaderTitle("AB",i)))
+    print(customHeaderTitle("AB",1))#Output BB
     print(customHeaderTitle("ABC",4))#Output AB
     print(customHeaderTitle("ABCD",83))#Output DDDD
-    
-    print(customHeaderTitle("LJVRKMMMNUXPRUP",9954554))
-    print(customHeaderTitle("MNOQ",22421))
+       
+    print(customHeaderTitle("MNOQ",22421))# Output
+    print(customHeaderTitle("LJVRKMMMNUXPRUP",9954554)) #Output RLNMMP
+    '''
+    print(customHeaderTitle("AB",1))#Output BB
+    print(customHeaderTitle("ABC",4))#Output AB
+    print(customHeaderTitle("ABCD",83))#Output DDD
+       
+    print(customHeaderTitle("MNOQ",22421))# Output MMMONMMN
+    print(customHeaderTitle("LJVRKMMMNUXPRUP",9954554)) #Output RLNMMP
     
     
